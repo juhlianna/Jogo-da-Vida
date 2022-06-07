c0d24cd9eedda293fb1896a4764cf741f042cf1c
jaei <juliana.panini@gft.com>
2022-06-07 11:56:46 -0300

    Initial commit

---
+++ test/teste/dojo/JogoDaVidaTest.java
@@ -0,0 +1,14 @@
+package teste.dojo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class JogoDaVidaTest {
+	
+	@Test
+	void Primeiro() throws Exception {
+		
+	}
+
+}
